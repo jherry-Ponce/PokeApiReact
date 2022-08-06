9f985b21054e07cc82e11b3fc0e38227951c3e1b
jherry-Ponce <jherry_libra23@hotmail.com>
2022-08-06 10:26:55 -0500

    update router

--- src/page/Header.jsx
+++ src/page/Header.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import '../styles.css';
 import { useState } from "react";
-import { Link, NavLink } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 import pokebolas from '../img/pokebolas.png'
 
 
@@ -30,26 +30,12 @@ const Header=()=>{
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 
-                <Link className="nav-link active" aria-current="page" to="PokeApiReact/">Pokemones</Link>
+                <Link className="nav-link active" aria-current="page" to="/">Pokemones</Link>
               </li>
-              {/* <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li> */}
             </ul>
             <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Buscar Pokemon"  aria-label="Search" onChange={handleCaptura}  />
-                 <Link  to={`PokeApiReact/Resul/${dato}`}> <button  className="btn btn-danger">Buscar</button> </Link>
+                 <Link  to={`Resul/${dato}`}> <button  className="btn btn-danger">Buscar</button> </Link>
             </form>
           </div>
         </div>
